a2b2262e636f5e2999c6df890c9313544deaaa18
Naidu <naidup.developer@gmail.com>
2020-01-07 17:28:18 +0530

    executed with switch

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    var softCooked = 5
+    var mediumCooked = 8
+    var hardCooked = 12
+    
+    @IBAction func onClick(_ sender: UIButton)
+    {
+        switch sender.currentTitle {
+        case "Soft":
+            print(softCooked)
+            break
+        case "Medium":
+            print(mediumCooked)
+            break
+        case "Hard":
+            print(hardCooked)
+            break
+        default:
+            break
+        }
+    }
 }
