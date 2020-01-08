6f62b9e885d5fda97482811edbc69087ccf3b11a
Naidu <naidup.developer@gmail.com>
2020-01-08 23:51:37 +0530

    Timer added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,28 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var softCooked = 5
-    var mediumCooked = 8
-    var hardCooked = 12
     
-    var eggTimes = ["Soft" : 5, "Medium" : 8, "Hard" : 12]
+    @IBOutlet weak var label: UILabel!
+    
+    var eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+    
+    var secondsRemaining : Int?
+    
+    var timer = Timer()
     
     @IBAction func onClick(_ sender: UIButton)
     {
-        print(eggTimes[sender.currentTitle!]!)
+        timer.invalidate()
+        secondsRemaining = eggTimes[sender.currentTitle!]
         
-//        switch sender.currentTitle {
-//        case "Soft":
-//            print(eggTimes.index(forKey: "soft") as Any)
-//            break
-//        case "Medium":
-//            print(eggTimes.index(forKey: "medium") as Any)
-//            break
-//        case "Hard":
-//            print(eggTimes.index(forKey: "hard") as Any)
-//            break
-//        default:
-//            break
-//        }
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true )
+        
+        
+        
+    }
+    
+    @objc func UpdateTimer() 
+    {
+        if secondsRemaining! > 0
+        {
+            print("\(secondsRemaining ?? 0) Seconds Remaining")
+            label.text = "wait for \(secondsRemaining ?? 0) Seconds"
+            secondsRemaining! -= 1
+        }else
+        {
+            timer.invalidate()
+            label.text = "Done..!!!"
+        }
     }
 }
